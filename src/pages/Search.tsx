0b20ff22030d3c11259472403e149cd6a498e6e3
toof <toof@toof.jp>
2024-11-26 23:45:02 +0900

    update

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -323,7 +323,7 @@ function Count({ count }: { count: CountJson | null }) {
 
   return (
     <div className="text-gray-800 prose prose-sm max-w-none prose-a:text-blue-500 prose-a:no-underline hover:prose-a:underline">
-      検索結果: {count.total_res_count}件 (書き込みID数: {count.unique_id_count}件)
+      検索結果: {count.total_res_count.toLocaleString()}件 (書き込みID数: {count.unique_id_count.toLocaleString()}件)
     </div>
   );
 }
